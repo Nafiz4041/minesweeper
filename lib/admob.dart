ed62ff603ae7f26384ca060bc6d9e964817407a7
Zaman Sheikh <zaman6545@gmail.com>
2024-11-24 23:08:35 +0600

    implement ads on game

---
+++ lib/admob.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+final List<String> testDeviceIds = [
+  "a32c5b41-837a-4ccb-a49f-85a7f10ddb8a",
+  "154dc959-1866-4f8a-8585-32c2c0f556a0",
+];
+
+const int maxFailedLoadAttempts = 3;
+//add request
+final AdRequest request = AdRequest(
+  keywords: <String>['foo', 'bar'],
+  contentUrl: 'http://foo.com/bar.html',
+  nonPersonalizedAds: true,
+);
+
+//intialize admob here
+void createRewardedInterstitialAd() {
+  RewardedInterstitialAd.load(
+      adUnitId: Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/5354046379'
+          : 'ca-app-pub-3940256099942544/6978759866',
+      request: request,
+      rewardedInterstitialAdLoadCallback: RewardedInterstitialAdLoadCallback(
+        onAdLoaded: (RewardedInterstitialAd ad) {
+          print('$ad loaded.');
+          rewardedInterstitialAd = ad;
+          numRewardedInterstitialLoadAttempts = 0;
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          print('RewardedInterstitialAd failed to load: $error');
+          rewardedInterstitialAd = null;
+          numRewardedInterstitialLoadAttempts += 1;
+          if (numRewardedInterstitialLoadAttempts < maxFailedLoadAttempts) {
+            createRewardedInterstitialAd();
+          }
+        },
+      ));
+}
+
+void showRewardedInterstitialAd() {
+  if (rewardedInterstitialAd == null) {
+    print('Warning: attempt to show rewarded interstitial before loaded.');
+    return;
+  }
+  rewardedInterstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+    onAdShowedFullScreenContent: (RewardedInterstitialAd ad) =>
+        print('$ad onAdShowedFullScreenContent.'),
+    onAdDismissedFullScreenContent: (RewardedInterstitialAd ad) {
+      print('$ad onAdDismissedFullScreenContent.');
+      ad.dispose();
+      createRewardedInterstitialAd();
+    },
+    onAdFailedToShowFullScreenContent:
+        (RewardedInterstitialAd ad, AdError error) {
+      print('$ad onAdFailedToShowFullScreenContent: $error');
+      ad.dispose();
+      createRewardedInterstitialAd();
+    },
+  );
+
+  rewardedInterstitialAd!.setImmersiveMode(true);
+  rewardedInterstitialAd!.show(
+      onUserEarnedReward: (AdWithoutView ad, RewardItem reward) {
+    print('$ad with reward $RewardItem(${reward.amount}, ${reward.type})');
+  });
+  rewardedInterstitialAd = null;
+}
+
+RewardedInterstitialAd? rewardedInterstitialAd;
+int numRewardedInterstitialLoadAttempts = 0;
---
+++ lib/main.dart
@@ -0,0 +1,502 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:minesweeper/admob.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Minesweeper',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[200],
+        fontFamily: 'Helvetica', // Using system font
+      ),
+      home: const LevelSelectionScreen(),
+      debugShowCheckedModeBanner: false, // Add this line
+    );
+  }
+}
+
+class LevelSelectionScreen extends StatefulWidget {
+  const LevelSelectionScreen({Key? key}) : super(key: key);
+
+  @override
+  _LevelSelectionScreenState createState() => _LevelSelectionScreenState();
+}
+
+class _LevelSelectionScreenState extends State<LevelSelectionScreen> {
+  int highestCompletedLevel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minesweeper Levels'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue[100]!, Colors.blue[50]!],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Select a Level',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[800],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(16),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 5,
+                    childAspectRatio: 1,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: 50,
+                  itemBuilder: (context, index) {
+                    final level = index + 1;
+                    final isUnlocked = level <= highestCompletedLevel + 1;
+                    return LevelButton(
+                      level: level,
+                      isUnlocked: isUnlocked,
+                      onPressed: isUnlocked
+                          ? () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => MinesweeperGame(
+                                    initialLevel: level,
+                                    onLevelComplete: (completedLevel) {
+                                      setState(() {
+                                        if (completedLevel >
+                                            highestCompletedLevel) {
+                                          highestCompletedLevel =
+                                              completedLevel;
+                                        }
+                                      });
+                                    },
+                                  ),
+                                ),
+                              );
+                            }
+                          : null,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LevelButton extends StatelessWidget {
+  final int level;
+  final bool isUnlocked;
+  final VoidCallback? onPressed;
+
+  const LevelButton({
+    Key? key,
+    required this.level,
+    required this.isUnlocked,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: isUnlocked ? 4 : 0,
+      borderRadius: BorderRadius.circular(12),
+      color: isUnlocked ? Colors.white : Colors.grey[300],
+      child: InkWell(
+        onTap: onPressed,
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: isUnlocked ? Colors.blue : Colors.grey,
+              width: 2,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              '$level',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: isUnlocked ? Colors.blue[700] : Colors.grey[600],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MinesweeperGame extends StatefulWidget {
+  final int initialLevel;
+  final Function(int) onLevelComplete;
+
+  const MinesweeperGame({
+    Key? key,
+    required this.initialLevel,
+    required this.onLevelComplete,
+  }) : super(key: key);
+
+  @override
+  _MinesweeperGameState createState() => _MinesweeperGameState();
+}
+
+class _MinesweeperGameState extends State<MinesweeperGame> {
+  late List<List<Cell>> grid;
+  bool isGameOver = false;
+  int revealedCount = 0;
+  late int currentLevel;
+  late int rows;
+  late int cols;
+  late int mineCount;
+
+  @override
+  void initState() {
+    super.initState();
+    currentLevel = widget.initialLevel;
+    _initializeLevel();
+    MobileAds.instance.updateRequestConfiguration(RequestConfiguration(
+        testDeviceIds: testDeviceIds));
+    createRewardedInterstitialAd();
+  }
+
+  void _initializeLevel() {
+    rows = 8 + (currentLevel ~/ 2);
+    cols = 8 + (currentLevel ~/ 2);
+    mineCount = 10 + currentLevel;
+    if (currentLevel > 50) {
+      currentLevel = 50;
+      rows = 33;
+      cols = 33;
+      mineCount = 60;
+    }
+    _initializeGame();
+  }
+
+  void _initializeGame() {
+    grid = List.generate(rows, (i) => List.generate(cols, (j) => Cell(i, j)));
+    _placeMines();
+    _calculateAdjacentMines();
+    revealedCount = 0;
+    isGameOver = false;
+  }
+
+  void _placeMines() {
+    Random random = Random();
+    int minesPlaced = 0;
+
+    while (minesPlaced < mineCount) {
+      int row = random.nextInt(rows);
+      int col = random.nextInt(cols);
+
+      if (!grid[row][col].isMine) {
+        grid[row][col].isMine = true;
+        minesPlaced++;
+      }
+    }
+  }
+
+  void _calculateAdjacentMines() {
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (!grid[i][j].isMine) {
+          grid[i][j].adjacentMines = _countAdjacentMines(i, j);
+        }
+      }
+    }
+  }
+
+  int _countAdjacentMines(int row, int col) {
+    int count = 0;
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        int newRow = row + i;
+        int newCol = col + j;
+        if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+          if (grid[newRow][newCol].isMine) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  void _revealCell(int row, int col) {
+    if (isGameOver || grid[row][col].isRevealed || grid[row][col].isFlagged) {
+      return;
+    }
+
+    setState(() {
+      grid[row][col].isRevealed = true;
+      revealedCount++;
+
+      if (grid[row][col].isMine) {
+        _gameOver(false);
+      } else if (grid[row][col].adjacentMines == 0) {
+        _revealAdjacentCells(row, col);
+      }
+
+      if (revealedCount == rows * cols - mineCount) {
+        _gameOver(true);
+      }
+    });
+  }
+
+  void _revealAdjacentCells(int row, int col) {
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        int newRow = row + i;
+        int newCol = col + j;
+        if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+          if (!grid[newRow][newCol].isRevealed &&
+              !grid[newRow][newCol].isMine) {
+            _revealCell(newRow, newCol);
+          }
+        }
+      }
+    }
+  }
+
+  void _toggleFlag(int row, int col) {
+    if (isGameOver || grid[row][col].isRevealed) {
+      return;
+    }
+
+    setState(() {
+      grid[row][col].isFlagged = !grid[row][col].isFlagged;
+    });
+  }
+
+  void _gameOver(bool isWin) {
+    isGameOver = true;
+    String message = isWin ? 'You Win!' : 'Game Over!';
+
+    if (isWin) {
+      widget.onLevelComplete(currentLevel);
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(message),
+          content: Text(isWin
+              ? 'Congratulations! You\'ve completed level $currentLevel.'
+              : 'Better luck next time!'),
+          actions: [
+            TextButton(
+              child: const Text('Back to Level Selection'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child:
+                  Text(isWin && currentLevel < 50 ? 'Next Level' : 'Try Again'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  if (isWin && currentLevel < 50) {
+                    currentLevel++;
+                  }
+                  _initializeLevel();
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minesweeper - Level $currentLevel'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                          child: Icon(Icons.movie),
+                          onTap: () {
+                            showRewardedInterstitialAd();
+                          })
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Mines: $mineCount',
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(width: 20),
+                      Text(
+                        'Grid: ${rows}x$cols',
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.9,
+                    maxHeight: MediaQuery.of(context).size.height * 0.7,
+                  ),
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: cols,
+                      childAspectRatio: 1,
+                      crossAxisSpacing: 2,
+                      mainAxisSpacing: 2,
+                    ),
+                    itemBuilder: (context, index) {
+                      int row = index ~/ cols;
+                      int col = index % cols;
+                      return GestureDetector(
+                        onTap: () => _revealCell(row, col),
+                        onLongPress: () => _toggleFlag(row, col),
+                        child: CellWidget(cell: grid[row][col]),
+                      );
+                    },
+                    itemCount: rows * cols,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Cell {
+  final int row;
+  final int col;
+  bool isMine;
+  bool isRevealed;
+  bool isFlagged;
+  int adjacentMines;
+
+  Cell(this.row, this.col)
+      : isMine = false,
+        isRevealed = false,
+        isFlagged = false,
+        adjacentMines = 0;
+}
+
+class CellWidget extends StatelessWidget {
+  final Cell cell;
+
+  const CellWidget({Key? key, required this.cell}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color cellColor = Colors.grey[300]!;
+    Widget cellContent = const SizedBox();
+
+    if (cell.isRevealed) {
+      cellColor = Colors.white;
+      if (cell.isMine) {
+        cellContent = const Icon(Icons.brightness_7, color: Colors.black);
+      } else if (cell.adjacentMines > 0) {
+        cellContent = Text(
+          '${cell.adjacentMines}',
+          style: TextStyle(
+            color: _getNumberColor(cell.adjacentMines),
+            fontWeight: FontWeight.bold,
+          ),
+        );
+      }
+    } else if (cell.isFlagged) {
+      cellContent = const Icon(Icons.flag, color: Colors.red);
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        color: cellColor,
+        border: Border.all(color: Colors.grey[400]!),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Center(child: cellContent),
+    );
+  }
+
+  Color _getNumberColor(int number) {
+    switch (number) {
+      case 1:
+        return Colors.blue;
+      case 2:
+        return Colors.green;
+      case 3:
+        return Colors.red;
+      case 4:
+        return Colors.purple;
+      case 5:
+        return Colors.brown;
+      case 6:
+        return Colors.cyan;
+      case 7:
+        return Colors.black;
+      case 8:
+        return Colors.grey;
+      default:
+        return Colors.black;
+    }
+  }
+}
